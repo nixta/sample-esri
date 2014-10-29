eef213c973bead6328455fd9d4fc473bd06c9d05
Marius Ursache <marius@marius.me.uk>
2014-10-29 20:19:46 +1100

    Added example that breaks loading a AGSWebMap by loading an empty layer before loading the map layer

--- Sample ArcGIS/ViewController.m
+++ Sample ArcGIS/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <ArcGIS/ArcGIS.h>
 
-@interface ViewController ()
+@interface ViewController ()<AGSWebMapDelegate>
+
+@property (nonatomic, strong) IBOutlet AGSMapView *mapView;
+@property (nonatomic, strong) AGSWebMap *esriWebMap;
 
 @end
 
@@ -17,11 +21,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Adding a blank layer before loading the AGSWebMap will break the loading of the map
+    // Also didOpenWebMap is no longer called. 
+    [self.mapView addMapLayer:[AGSGraphicsLayer graphicsLayer]];
+    
+    self.esriWebMap = [AGSWebMap webMapWithItemId:@"d802f08316e84c6592ef681c50178f17" credential:nil];
+    self.esriWebMap.delegate = self;
+    self.esriWebMap.zoomToDefaultExtentOnOpen = YES;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - AGSWebMapDelegate
+
+- (void)webMapDidLoad:(AGSWebMap *)webMap {
+    [self.esriWebMap openIntoMapView:self.mapView withAlternateBaseMap:nil resetMapView:NO];
+    NSLog(@"webMapDidLoad");
+}
+
+- (void)webMap:(AGSWebMap *)webMap didLoadLayer:(AGSLayer *)layer {
+    NSLog(@"layer loaded");
+}
+
+- (void)didOpenWebMap:(AGSWebMap *)webMap intoMapView:(AGSMapView *)mapView {
+    NSLog(@"didOpenWebMap");
 }
 
 @end
